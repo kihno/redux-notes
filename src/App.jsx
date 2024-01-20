a507144ca7d17b9fd1c7ffd2ed2657ca5f1a74e1
kihno <stistyle13@gmail.com>
2024-01-19 16:54:49 -0800

    Refactor notes list and form into separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,12 @@
-import { createStore } from 'redux'
-import noteReducer from './reducers/noteReducer'
-import { createNote, toggleImportanceOf } from './actions'
-
-const store = createStore(noteReducer)
-
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
+import NewNote from './components/NewNote'
+import Notes from './components/Notes'
 
 const App = () => {
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    store.dispatch(createNote(content))
-  }
 
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id))
-  }
-
-  return(
+  return (
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map(note =>
-          <li key={note.id} onClick={() => toggleImportance(note.id)}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from './App.jsx'
+import noteReducer from './reducers/noteReducer.js'
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
 )
